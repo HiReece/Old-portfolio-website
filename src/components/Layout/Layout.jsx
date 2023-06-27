9a512943390d3edc3787d4cba497875ed72123fa
hiReece <hireece@proton.me>
2023-06-27 12:27:47 +0300

    Navbar added as component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,9 @@ import * as React from "react"
 import { useEffect, useLayoutEffect, useState, useRef } from "react"
 import * as layoutStyles from "./Layout.module.css"
 import "../../styles/reset.css"
-import {routes} from "../../routes.json"
 import { graphql, useStaticQuery } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
-
+import Navbar from "../Navbar/Navbar"
 
 const Layout = () => {
   const data = useStaticQuery(graphql`
@@ -51,7 +50,7 @@ const Layout = () => {
   const [footerPolygonRotationDegrees, setFooterPolygonRotationDegrees] = useState(0)
   const [navbarHidden, setNavbarHidden] = useState(false)
   const [isNavbarAtTheTop, setNavbarAtTheTop] = useState(true)
-  const [isMobileNavOpened, setIsMobileNavOpened] = useState(false)
+
   const [portfolioImage, setPortfolioImage] = useState("mobileImage")
   const greetings = ["hello", "labas", "bonjour", "hallo", "ciao", "привіт", "hola", "habari", "こんにちは", "witam", "ahoj", "tere", "hallå", "hei", "你好", "merhaba"]
   const [greetingIndex, setGreetingIndex] = useState(0)
@@ -59,7 +58,7 @@ const Layout = () => {
   const footerPolygonRef = useRef()
   let [prevScrollPos, setPrevScrollPos] = useState(0)
 
-  const toggleMobileNav = () => setIsMobileNavOpened(!isMobileNavOpened)
+
 
    const [windowWidth, setWindowWidth] = useState(() => {
     if (typeof window !== "undefined") {
@@ -135,31 +134,7 @@ const Layout = () => {
  
     return (
         <div className={layoutStyles.container}>
-          <div className={`${layoutStyles.navbarContainer} ${navbarHidden ? layoutStyles.itemHidden : ""} ${isMobileNavOpened ? layoutStyles.navbarContainerFullHeight : ""}`}>
-            <ul className={`${layoutStyles.navbarLinks} ${isMobileNavOpened ? layoutStyles.navOpened : ""}`}>
-              {Object.keys(routes).map( route => (
-                <li className={`${layoutStyles.navbarLinkItem} ${isNavbarAtTheTop ? layoutStyles.navbarAtTheTop : ""}`} onClick={isMobileNavOpened ? toggleMobileNav : null} key={routes[route].id}>
-                  <a className={layoutStyles.navbarLink} href={routes[route].link}>
-                    {routes[route].name}
-                  </a>
-                </li>
-              ))
-              }
-            </ul> 
-              {isMobileNavOpened ? 
-                <button className={layoutStyles.closeButton} onClick={toggleMobileNav}>
-                  <svg className={layoutStyles.closeIcon} viewBox="0 0 32 32">
-                    <g id="cross"><line className={layoutStyles.iconStroke} x1="7" x2="25" y1="7" y2="25"/><line className={layoutStyles.iconStroke} x1="7" x2="25" y1="25" y2="7"/></g>
-                  </svg>
-                </button> :
-                <button className={`${layoutStyles.menuButton} ${navbarHidden ? layoutStyles.itemHidden : ""}`} onClick={toggleMobileNav}>
-                  <svg className={layoutStyles.menuIcon} viewBox="0 0 24 24" >
-                    <line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="18" y2="18"/>
-                  </svg>
-                </button>
-              }
-          </div>
-          
+          <Navbar navbarHidden={navbarHidden} isNavbarAtTheTop={isNavbarAtTheTop} />
           <main>
             <div className={layoutStyles.heroContainer} id="hero">
               <div className={layoutStyles.heroHeadingContainer}>
@@ -286,7 +261,7 @@ const Layout = () => {
               </div>
             </section>
 
-            {!isMobileNavOpened && !isNavbarAtTheTop ? <div className={layoutStyles.scrollToTopContainer}>
+            {!isNavbarAtTheTop ? <div className={layoutStyles.scrollToTopContainer}>
               <a className={layoutStyles.scrollToTop} href="#hero">
               <svg className={layoutStyles.scrollTopIcon} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.2652 3 12.5196 3.10536 12.7071 3.29289L19.7071 10.2929C20.0976 10.6834 20.0976 11.3166 19.7071 11.7071C19.3166 12.0976 18.6834 12.0976 18.2929 11.7071L13 6.41421V20C13 20.5523 12.5523 21 12 21C11.4477 21 11 20.5523 11 20V6.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L11.2929 3.29289C11.4804 3.10536 11.7348 3 12 3Z" />
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+import { useState } from "react"
+import {navbarContainer, navbarContainerFullHeight, navbarLinks,
+        navbarLinkItem, navbarLink, navbarAtTheTop,
+        navOpened, itemHidden, closeButton,
+        closeIcon, menuButton, menuIcon } from "./Navbar.module.css"
+
+import {routes} from "../../routes.json"
+
+const Navbar = ({navbarHidden, isNavbarAtTheTop}) => {
+    const [isMobileNavOpened, setIsMobileNavOpened] = useState(false)
+    const toggleMobileNav = () => setIsMobileNavOpened(!isMobileNavOpened)
+
+    return(
+        <div className={`${navbarContainer} ${navbarHidden ? itemHidden : ""} ${isMobileNavOpened ? navbarContainerFullHeight : ""}`}>
+            <ul className={`${navbarLinks} ${isMobileNavOpened ? navOpened : ""}`}>
+                {Object.keys(routes).map( route => (
+                    <li className={`${navbarLinkItem} ${isNavbarAtTheTop ? navbarAtTheTop : ""}`} onClick={isMobileNavOpened ? toggleMobileNav : null} key={routes[route].id}>
+                        <a className={navbarLink} href={routes[route].link}>
+                            {routes[route].name}
+                        </a>
+                    </li>
+                ))}
+            </ul> 
+        {isMobileNavOpened ? 
+            <button className={closeButton} onClick={toggleMobileNav}>
+                <svg className={closeIcon} viewBox="0 0 32 32">
+                   <g id="cross"><line x1="7" x2="25" y1="7" y2="25"/><line x1="7" x2="25" y1="25" y2="7"/></g>
+                </svg>
+            </button> :
+            <button className={`${menuButton} ${navbarHidden ? itemHidden : ""}`} onClick={toggleMobileNav}>
+                <svg className={menuIcon} viewBox="0 0 24 24" >
+                    <line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="18" y2="18"/>
+                </svg>
+            </button>
+        }
+</div>
+    )
+}
+
+export default Navbar
